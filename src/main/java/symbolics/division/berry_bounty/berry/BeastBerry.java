bd387a7f5a517bbaffadc8c23f7885eee37703ef
Jonathan Vuong <jonathanvuong4@yahoo.com>
2025-01-01 19:44:25 -0600

    Update BeastBerry.java
    hunger effect is buffed, consumers lose even more food from eating beast berries

--- src/main/java/symbolics/division/berry_bounty/berry/BeastBerry.java
+++ src/main/java/symbolics/division/berry_bounty/berry/BeastBerry.java
@@ -13,10 +13,10 @@ public class BeastBerry extends Berry {
     @Override
     //the standard effect given to the user
     public void strongEffect(LivingEntity target) {
-        //give consumer HUNGER 120 !!!! for 1 second :p
-        //honnggrrryyyyy (reduces saturation by 3, for a net total of +2.4-3= -0.6 saturation overall)
+        //give consumer HUNGER 160 !!!! for 1 second :p
+        //honnggrrryyyyy (reduces saturation by 4, for a net total of +2.4-4= -1.6 saturation overall)
         //this berry makes you hungrier than before you ate it...
-        target.addStatusEffect(new StatusEffectInstance(StatusEffects.HUNGER, 20, 119));
+        target.addStatusEffect(new StatusEffectInstance(StatusEffects.HUNGER, 20, 159));
         //give strength 3 for 3 seconds (+9 damage per hit !! yikes)
         target.addStatusEffect(new StatusEffectInstance(StatusEffects.STRENGTH, 60, 2));
     }
@@ -24,9 +24,9 @@ public class BeastBerry extends Berry {
     @Override
     //what happens to all entities affected by the user eating this berry with convection
     public void weakEffect(LivingEntity target) {
-        //give consumer HUNGER 80 !!!! for 1 second :p
-        //honnggrrryyyyy (reduces saturation by 2, for a net total of +2.4-2= +0.4 saturation overall)
-        target.addStatusEffect(new StatusEffectInstance(StatusEffects.HUNGER, 20, 79));
+        //give consumer HUNGER 120 !!!! for 1 second :p
+        //honnggrrryyyyy (reduces saturation by 3, for a net total of +2.4-3= -0.6 saturation overall)
+        target.addStatusEffect(new StatusEffectInstance(StatusEffects.HUNGER, 20, 119));
         //give strength 2 for 3 seconds (+6 damage per hit !! yikes)
         target.addStatusEffect(new StatusEffectInstance(StatusEffects.STRENGTH, 60, 1));
     }
